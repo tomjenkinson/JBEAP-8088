a8153eaaf484027b35f2fa3a29324e6bfab14e34
Peter Palaga <ppalaga@redhat.com>
2017-02-06 13:58:29 +0100

    Srcdeps against current WF master

--- itest/src/test/java/org/wildfly/jbeap8088/RemoteEJBClientITest.java
+++ itest/src/test/java/org/wildfly/jbeap8088/RemoteEJBClientITest.java
@@ -16,14 +16,12 @@
  */
 package org.wildfly.jbeap8088;
 
-import java.util.Hashtable;
 import java.util.Properties;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
-import org.jboss.as.quickstarts.ejb.remote.stateful.RemoteCounter;
 import org.jboss.logging.Logger;
 import org.junit.Test;
 
@@ -52,40 +50,4 @@ public class RemoteEJBClientITest {
         ctx.close();
     }
 
-    /**
-     * Looks up and returns the proxy to remote stateful counter bean
-     *
-     * @return
-     * @throws NamingException
-     */
-    private static RemoteCounter lookupRemoteStatefulCounter() throws NamingException {
-
-        final Hashtable<String, String> jndiProperties = new Hashtable<String, String>();
-
-        jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
-        jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.as.naming.InitialContextFactory");
-        final Context context = new InitialContext(jndiProperties);
-
-        // The JNDI lookup name for a stateful session bean has the syntax of:
-        // ejb:<appName>/<moduleName>/<distinctName>/<beanName>!<viewClassName>?stateful
-        //
-        // <appName> The application name is the name of the EAR that the EJB is deployed in
-        // (without the .ear). If the EJB JAR is not deployed in an EAR then this is
-        // blank. The app name can also be specified in the EAR's application.xml
-        //
-        // <moduleName> By the default the module name is the name of the EJB JAR file (without the
-        // .jar suffix). The module name might be overridden in the ejb-jar.xml
-        //
-        // <distinctName> : AS7 allows each deployment to have an (optional) distinct name.
-        // This example does not use this so leave it blank.
-        //
-        // <beanName> : The name of the session been to be invoked.
-        //
-        // <viewClassName>: The fully qualified classname of the remote interface. Must include
-        // the whole package name.
-
-        // let's do the lookup
-        return (RemoteCounter) context.lookup("ejb:/jboss-ejb-remote-server-side/CounterBean!"
-                + RemoteCounter.class.getName() + "?stateful");
-    }
 }
